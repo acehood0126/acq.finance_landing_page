6b2739d8227ac3efa412d67795a0bf37ffc7f58f
acehood0126 <acehood010126@gmail.com>
2022-11-01 13:21:58 +0000

    fix/remove_toolbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState } from "react";
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {useNavigate} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import Home from './pages/home'
-import Products from './pages/products'
-import AboutUs from './pages/aboutus'
-import Buy from './pages/buy'
+import Home from "./pages/home";
+import Products from "./pages/products";
+import AboutUs from "./pages/aboutus";
+import Buy from "./pages/buy";
 
-import logo from './assets/logo.png'
+import logo from "./assets/logo.png";
 
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/ToolBar'
-import Button from '@mui/material/Button'
-import Grid from '@mui/material/Grid'
-import Image from 'mui-image'
-import { Typography, useMediaQuery, Menu, MenuItem } from '@mui/material';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+// import Toolbar from '@mui/material/ToolBar'
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Image from "mui-image";
+import { Typography, useMediaQuery, Menu, MenuItem } from "@mui/material";
 
-import { useTheme } from '@mui/material/styles';
+import { useTheme } from "@mui/material/styles";
 
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 
 function App() {
-  const navItems = ['Products', 'About Us', 'Buy']
+  const navItems = ["Products", "About Us", "Buy"];
 
   const theme = useTheme();
-  const isMobile = !useMediaQuery(theme.breakpoints.up('sm'));
+  const isMobile = !useMediaQuery(theme.breakpoints.up("sm"));
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
@@ -38,12 +38,12 @@ function App() {
   };
   const handleClose = () => {
     setAnchorEl(null);
-  }
+  };
 
   return (
     <Fragment>
-        <AppBar position="fixed" component="nav" sx={{bgcolor: "white"}}>
-          <Toolbar>
+      <AppBar position="fixed" component="nav" sx={{ bgcolor: "white" }}>
+        {/* <Toolbar>
             <Box px={1}>
                 <Image src={logo} height={40}/>
             </Box>
@@ -80,30 +80,33 @@ function App() {
                     ))}
                 </Box>
             }
-          </Toolbar>
-        </AppBar>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/aboutus" element={<AboutUs />} />
-            <Route path="/buy" element={<Buy />} />
-        </Routes>
-        <AppBar position="static" sx={{ bgcolor: '#25F522', top: 'auto', bottom: 0 }}>
-            <Box p={isMobile?3:5} px={isMobile?2:10} mt={isMobile?0:2}>
-                <Grid container spacing={2}>
-                    <Grid item xs={6}>
-                        <Typography>Contact</Typography>
-                        <Typography>About Us</Typography>
-                        <Typography>Terms & Conditions</Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Typography>Careers</Typography>
-                        <Typography>Change country</Typography>
-                        <Typography>FAQ</Typography>
-                    </Grid>
-                </Grid>
-            </Box>
-        </AppBar>
+          </Toolbar> */}
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/buy" element={<Buy />} />
+      </Routes>
+      <AppBar
+        position="static"
+        sx={{ bgcolor: "#25F522", top: "auto", bottom: 0 }}
+      >
+        <Box p={isMobile ? 3 : 5} px={isMobile ? 2 : 10} mt={isMobile ? 0 : 2}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Typography>Contact</Typography>
+              <Typography>About Us</Typography>
+              <Typography>Terms & Conditions</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography>Careers</Typography>
+              <Typography>Change country</Typography>
+              <Typography>FAQ</Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </AppBar>
     </Fragment>
   );
 }
